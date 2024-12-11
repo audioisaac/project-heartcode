17103e37c91ce9822cd911e0a2479ebe14af8383
Pharoleon <ng_shiloh@students.edu.sg>
2024-12-11 14:32:36 +0800

    hehe

--- next.config.ts
+++ next.config.ts
@@ -8,3 +8,11 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+// next.config.js
+module.exports = {
+  images: {
+    domains: ['images.unsplash.com'], // Allow images from Unsplash
+  },
+}
+
