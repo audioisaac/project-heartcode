ee22c2d8df8239546ddb1245f696a65a9a1056c0
Pharoleon <ng_shiloh@students.edu.sg>
2024-12-11 13:49:51 +0800

    latest

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Image from "next/image";
 import { Button } from "@/components/ui/button"
 import {
@@ -9,6 +11,7 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 import { Navigation, Terminal } from "lucide-react"
+import { z } from "zod"
 
 import {
   Alert,
@@ -47,25 +50,83 @@ import {
   NavigationMenuViewport,
 } from "@/components/ui/navigation-menu"
 
-export default function Quiz() {
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+})
+
+// export default function Quiz() {
+//   return (
+//     <div>
+//       {/* START CODING HERE */}
+//       <p>Methamphetamine, heroin, and cannabis were the three most commonly 
+// abused drugs in Singapore. 1,621 methamphetamine abusers (52%), 1,039 heroin 
+// abusers (33%), and 276 cannabis abusers (9%) were arrested in 2023.</p>
+
+// <p>Negative effects of drugs include:
+// Associated health issues such as lung or heart disease, stroke, cancer, or mental health conditions.
+// Damage to the brain, heart, liver, lungs, and kidneys.
+// Increased risk of cancer.
+// Abnormal vital signs, sleep changes, decreased memory, and cognitive abilities.
+// Nausea, vomiting, diarrhea, stomach pain.
+// Legal consequences, financial problems, injuries, and death.</p>
+
+//       {/* END CODING HERE */}
+//     </div>
+//   );
+
+// }
+
+export default function ProfileForm() {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+
   return (
-    <div>
-
-      {/* START CODING HERE */}
-      <p>Methamphetamine, heroin, and cannabis were the three most commonly 
-abused drugs in Singapore. 1,621 methamphetamine abusers (52%), 1,039 heroin 
-abusers (33%), and 276 cannabis abusers (9%) were arrested in 2023.</p>
-
-<p>Negative effects of drugs include:
-Associated health issues such as lung or heart disease, stroke, cancer, or mental health conditions.
-Damage to the brain, heart, liver, lungs, and kidneys.
-Increased risk of cancer.
-Abnormal vital signs, sleep changes, decreased memory, and cognitive abilities.
-Nausea, vomiting, diarrhea, stomach pain.
-Legal consequences, financial problems, injuries, and death.</p>
-
-      {/* END CODING HERE */}
-    </div>
-  );
-
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  )
 }
