9e776a027defc76dd14cd6786930d54f6f3d810c
Pharoleon <ng_shiloh@students.edu.sg>
2024-12-11 16:31:03 +0800

    shilohwednesday

--- src/app/about_me/page.tsx
+++ src/app/about_me/page.tsx
@@ -59,7 +59,7 @@ export default function Home() {
   return (
     <div className="w-full h-full py-20">
       <h2 className="max-w-7xl pl-4 mx-auto text-xl md:text-5xl font-bold text-neutral-800 dark:text-neutral-200 font-sans">
-        Get to know your iSad.
+        Don't be a Drug addict.
       </h2>
       <Carousel items={cards} />
     </div>
@@ -71,7 +71,7 @@ export default function Home() {
 
 
 
-const DummyContent = () => {
+const CocaineContent = () => {
   return (
     <>
       {[...new Array(3).fill(1)].map((_, index) => {
@@ -82,12 +82,72 @@ const DummyContent = () => {
           >
             <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto">
               <span className="font-bold text-neutral-700 dark:text-neutral-200">
-                The first rule of Apple club is that you boast about Apple club.
+                Information on COCAINE:
               </span>{" "}
-              Keep a journal, quickly jot down a grocery list, and take amazing
-              class notes. Want to convert those notes to text? No problem.
-              Langotiya jeetu ka mara hua yaar is ready to capture every
-              thought.
+              Cocaine use can lead to severe health issues such as heart attack, stroke, addiction, and mental health disorders. Legal consequences include imprisonment, hefty fines, and criminal records, affecting employment and social status.
+            </p>
+            <Image
+              src={Cocaine}
+              alt="Macbook mockup from Aceternity UI"
+              height="500"
+              width="500"
+              className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain"
+            />
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+
+
+const FentanylContent = () => {
+  return (
+    <>
+      {[...new Array(3).fill(1)].map((_, index) => {
+        return (
+          <div
+            key={"dummy-content" + index}
+            className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4"
+          >
+            <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto">
+              <span className="font-bold text-neutral-700 dark:text-neutral-200">
+                Information on FENTANYL:
+              </span>{" "}
+              Fentanyl use can cause respiratory failure, overdose, addiction, and death. Legal consequences include severe criminal charges, long prison sentences, and substantial fines. Possession and trafficking carry particularly harsh penalties due to fentanyl's high potency.
+            </p>
+            <Image
+              src={Cocaine}
+              alt="Macbook mockup from Aceternity UI"
+              height="500"
+              width="500"
+              className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain"
+            />
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+
+
+
+const CannabisContent = () => {
+  return (
+    <>
+      {[...new Array(3).fill(1)].map((_, index) => {
+        return (
+          <div
+            key={"dummy-content" + index}
+            className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4"
+          >
+            <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto">
+              <span className="font-bold text-neutral-700 dark:text-neutral-200">
+                Information on CANNABIS:
+              </span>{" "}
+              Cannabis use can cause memory issues, impaired coordination, anxiety, and addiction. Legal consequences vary by location, with fines, criminal charges, and imprisonment for illegal possession or distribution. Some regions impose strict penalties for trafficking.
             </p>
             <Image
               src={Cocaine}
@@ -106,40 +166,21 @@ const DummyContent = () => {
 const data = [
   {
     category: "Cocaine",
-    title: "You can do more with AI.",
+    title: "Why do Cocaine when you can do much more?",
     src: Cocaine,
-    content: <DummyContent />,
+    content: <CocaineContent />,
   },
   {
     category: "Fentanyl",
-    title: "Enhance your productivity.",
+    title: "Diminish your productivity.",
     src: Cocaine,
-    content: <DummyContent />,
+    content: <FentanylContent />,
   },
   {
-    category: "   ",
-    title: "Launching the new Apple Vision Pro.",
+    category: "Cannabis",
+    title: "Drugs = Bad",
     src: Cocaine,
-    content: <DummyContent />,
-  },
-
-  {
-    category: "Product",
-    title: "Maps for your iPhone 15 Pro Max.",
-    src: Cocaine,
-    content: <DummyContent />,
-  },
-  {
-    category: "iOS",
-    title: "Photography just got better.",
-    src: Cocaine,
-    content: <DummyContent />,
-  },
-  {
-    category: "Hiring",
-    title: "Hiring for a Staff Software Engineer",
-    src: Cocaine,
-    content: <DummyContent />,
-  },
+    content: <CannabisContent />,
+  }
 ];
 
